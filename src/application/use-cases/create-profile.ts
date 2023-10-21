5697edcfd34509af40a6b5c90e6f437c5d091338
lucas cortez <lucasc.sanches65@gmail.com>
2023-10-21 20:08:42 -0300

    feat: max of 3 profiles per user

--- src/application/use-cases/create-profile.ts
+++ src/application/use-cases/create-profile.ts
@@ -1,3 +1,4 @@
+import { AppError } from "application/errors/app-error";
 import { IUseCase } from "core/contracts/usecase";
 import { ProfileEntity } from "core/domain/entities/profile";
 import { ProfileRepository } from "core/domain/repositories/profile.repository";
@@ -16,6 +17,10 @@ export class CreateProfileUseCase implements IUseCase<CreateProfileInput, Create
   constructor(private readonly profileRepository: ProfileRepository) {}
 
   async execute(input: CreateProfileInput): Promise<CreateProfileOutput> {
+    const profiles = await this.profileRepository.getProfilesFromUser(input.userId);
+
+    if (profiles.length >= 3) throw new AppError({ statusCode: 406, message: "3 profiles allowed" });
+
     const profileEntity = ProfileEntity.create(
       input.name,
       input.interval,
